3ebb7d9a095a24744db84790679f33727dc60352
Bakhtiyor Ganijon <bkhtdev@Bakhtiyors-Mac-mini.local>
2023-09-10 08:01:53 +0900

    Fix nested layout problem

--- app/(Auth)/signin/page.tsx
+++ app/(Auth)/signin/page.tsx
@@ -35,9 +35,9 @@ export default function SigninPage() {
       <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
+          <Link href="/" className="relative z-20 flex items-center text-lg font-medium">
             <Icons.logo />
-          </div>
+          </Link>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
