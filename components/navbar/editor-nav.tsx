dceceb4436d690e9a24bf6c9059f8404282d08a5
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-10 08:55:26 +0900

    test commit

--- components/navbar/editor-nav.tsx
+++ components/navbar/editor-nav.tsx
@@ -13,7 +13,7 @@ export default function EditorNavbar() {
                <div className="menu-backdrop h-[60px] border-b w-full">
                </div>
                <div className="menu-container p-3 xl:px-36 2xl:px-64">
-                    <Button variant="ghost" size={"sm"} asChild>
+                    <Button variant="ghost" size={""} asChild>
                          <Link href="/user" className="flex align-items-center">
                               <Avatar className="h-8 w-8 mr-3">
                                    <AvatarImage src="https://avatars.githubusercontent.com/u/62228656" alt="@yusupovbg" />
