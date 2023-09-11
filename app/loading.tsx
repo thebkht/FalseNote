fc406591c37cf84a2f8354f0a09d026618a63aa4
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-11 16:12:49 +0900

    Update loading screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,8 +2,8 @@ import { Icons } from "@/components/icon";
 
 export default function Home(){
      return (
-          <div className="w-full h-screen flex">
-         <Icons.logo className="w-56 h-28 animate-pulse m-auto align-middle" />
+          <div className="w-full h-screen flex justify-center items-center">
+         <Icons.logo className="w-56 h-28 animate-pulse" />
        </div>
      )
 }
