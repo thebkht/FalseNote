cff4d3f4680157581e87b198fb4986e30106e0a6
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-12 10:28:36 +0900

    Delete app/api/auth/[...nextauth].ts

--- app/api/auth/[...nextauth].ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-import NextAuth from "next-auth"
-import { options } from "./options";
-
-export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-  // Do whatever you want here, before the request is passed down to `NextAuth`
-  return await NextAuth(req, res, options)
-}
