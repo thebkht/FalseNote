a5d384d702f68822d906059c88996ad29bb32177
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-12 09:49:24 +0900

    Update [...nextauth].ts

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
-import NextAuth from "next-auth/next";
+import type { NextApiRequest, NextApiResponse } from "next"
+import NextAuth from "next-auth"
 import { options } from "./options";
 
-const handler = NextAuth(options)
-
-export { handler as GET, handler as POST };
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  // Do whatever you want here, before the request is passed down to `NextAuth`
+  return await NextAuth(req, res, options)
+}
