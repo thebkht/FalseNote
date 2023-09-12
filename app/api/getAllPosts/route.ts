f76c03cd9cb01ea56a88503f40e29d72b6fef2b8
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-12 20:54:22 +0900

    Add fetch post and user api

---
+++ app/api/getAllPosts/route.ts
@@ -0,0 +1,15 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  try {
+    // Execute a query to fetch all table names
+    const result = await sql`
+      SELECT * FROM blogposts
+    `;
+
+    return NextResponse.json({ result }, { status: 200});
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
