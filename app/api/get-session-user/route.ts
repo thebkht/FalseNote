b14dfcdf9297295286b02ddc12ad39a207bc9003
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-13 22:29:17 +0900

    Update

--- app/api/get-session-user/route.ts
+++ app/api/get-session-user/route.ts
@@ -1,18 +1,30 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
-import { useSession } from 'next-auth/react'
+import { getSession } from 'next-auth/react'; // Remove GetSessionParams
 
-export async function GET(request: Request) {
+export async function getServerSideProps(context: any) {
   try {
-     const session = useSession();
-     const name = session.data?.user?.name;
-    // Execute a query to fetch all table names
+    const { req, res } = context;
+
+    // Get the user's session from the server-side
+    const session = await getSession({ req });
+
+    // Retrieve the user's name from the session
+    const name = session?.user?.name;
+
+    if (!name) {
+      // Handle the case where the user is not authenticated
+      return NextResponse.json({ error: 'User not authenticated' }, { status: 401 });
+    }
+
+    // Execute a query to fetch data based on the user's name
     const result = await sql`
       SELECT * FROM users WHERE Name = ${name} OR Username = ${name}
     `;
 
-    return NextResponse.json({ result }, { status: 200});
+    return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+    console.error('Error:', error);
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
   }
 }
