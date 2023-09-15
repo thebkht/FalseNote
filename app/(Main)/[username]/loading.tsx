40de3fc5576e133b1774db14a83eeb41338c9970
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-16 07:43:57 +0900

    Add feed skeleton

--- app/(Main)/[username]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Icons } from "@/components/icon";
-
-export default function Loading(){
-     return (
-          <div className="w-full h-screen flex justify-center items-center bg-background">
-         <Icons.logo className="w-56 h-28 animate-pulse" />
-       </div>
-     )
-}
--- components/feed/featured/featured-dev.tsx
+++ components/feed/featured/featured-dev.tsx
@@ -8,15 +8,18 @@ import { Key } from "react";
 import { useEffect, useState } from "react";
 import { getFeaturedDevs } from "@/components/get-user";
 import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function FeaturedDev() {
   const [featuredDevs, setFeaturedDevs] = useState([]);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const userData = await getFeaturedDevs();
         setFeaturedDevs(userData.users);
+        setIsLoaded(true);
       } catch (error) {
         // Handle errors
         console.error('Error:', error);
@@ -29,7 +32,7 @@ export default function FeaturedDev() {
   let content = null;
 
   if (Array.isArray(featuredDevs)) {
-    content = (
+    isLoaded ? content = (
       <Card className="feed__empty_featured_card">
         <CardHeader>
           <CardTitle className="feed__empty_featured_card_title">Featured Devs</CardTitle>
@@ -38,21 +41,21 @@ export default function FeaturedDev() {
           <div className="feed__empty_featured_card_content flex flex-col items-start justify-between space-y-4">
             {featuredDevs.map((item: { userId: Key | null | undefined; profilepicture: string | undefined; username: string | undefined; name: string | undefined; bio: string | undefined; }) => (
               <div className="flex gap-4 w-full items-center justify-between" key={item.userId}>
-                <Link href={`/${item.username}`}  className="flex items-center">
-                <Avatar className="h-10 w-10 mr-4">
-                      <AvatarImage src={item.profilepicture} alt={item.username} />
-                      <AvatarFallback>{item.name?.charAt(0) || item.username?.charAt(0)}</AvatarFallback>
-                    </Avatar>
+                <Link href={`/${item.username}`} className="flex items-center">
+                  <Avatar className="h-10 w-10 mr-4">
+                    <AvatarImage src={item.profilepicture} alt={item.username} />
+                    <AvatarFallback>{item.name?.charAt(0) || item.username?.charAt(0)}</AvatarFallback>
+                  </Avatar>
                   {
                     item.name === null ? (
                       <div>
-                    <p className="text-sm font-medium leading-none">@{item.username}</p>
-                  </div>
+                        <p className="text-sm font-medium leading-none">@{item.username}</p>
+                      </div>
                     ) : (
                       <div>
-                    <p className="text-sm font-medium leading-none">{item.name}</p>
-                    <p className="text-sm text-muted-foreground">@{item.username}</p>
-                  </div>
+                        <p className="text-sm font-medium leading-none">{item.name}</p>
+                        <p className="text-sm text-muted-foreground">@{item.username}</p>
+                      </div>
                     )
                   }
                 </Link>
@@ -65,7 +68,81 @@ export default function FeaturedDev() {
           </div>
         </CardContent>
       </Card>
-    );
+    ) : (
+      content = <Card className="feed__empty_featured_card">
+        <CardHeader>
+          <Skeleton className="h-6 w-44" />
+        </CardHeader>
+        <CardContent>
+          <div className="feed__empty_featured_card_content flex flex-col items-start justify-between space-y-4">
+            <div className="flex gap-4 w-full items-center justify-between">
+              <div className="space-y-3">
+              <div className="flex items-center">
+                <Skeleton className="h-10 w-10 rounded-full mr-4" />
+                <div className="space-y-2">
+                  <Skeleton className="h-4 w-48" />
+                  <Skeleton className="h-4 w-36" />
+                </div>
+              </div>
+              <Skeleton className="h-4 w-full" />
+              </div>
+              <Skeleton className="h-10 w-32" />
+            </div>
+            <div className="flex gap-4 w-full items-center justify-between">
+              <div className="space-y-3">
+              <div className="flex items-center">
+                <Skeleton className="h-10 w-10 rounded-full mr-4" />
+                <div className="space-y-2">
+                  <Skeleton className="h-4 w-48" />
+                  <Skeleton className="h-4 w-36" />
+                </div>
+              </div>
+              <Skeleton className="h-4 w-full" />
+              </div>
+              <Skeleton className="h-10 w-32" />
+            </div>
+            <div className="flex gap-4 w-full items-center justify-between">
+              <div className="space-y-3">
+              <div className="flex items-center">
+                <Skeleton className="h-10 w-10 rounded-full mr-4" />
+                <div className="space-y-2">
+                  <Skeleton className="h-4 w-48" />
+                  <Skeleton className="h-4 w-36" />
+                </div>
+              </div>
+              <Skeleton className="h-4 w-full" />
+              </div>
+              <Skeleton className="h-10 w-32" />
+            </div>
+            <div className="flex gap-4 w-full items-center justify-between">
+              <div className="space-y-3">
+              <div className="flex items-center">
+                <Skeleton className="h-10 w-10 rounded-full mr-4" />
+                <div className="space-y-2">
+                  <Skeleton className="h-4 w-48" />
+                  <Skeleton className="h-4 w-36" />
+                </div>
+              </div>
+              <Skeleton className="h-4 w-full" />
+              </div>
+              <Skeleton className="h-10 w-32" />
+            </div>
+            <div className="flex gap-4 w-full items-center justify-between">
+              <div className="space-y-3">
+              <div className="flex items-center">
+                <Skeleton className="h-10 w-10 rounded-full mr-4" />
+                <div className="space-y-2">
+                  <Skeleton className="h-4 w-48" />
+                  <Skeleton className="h-4 w-36" />
+                </div>
+              </div>
+              <Skeleton className="h-4 w-full" />
+              </div>
+              <Skeleton className="h-10 w-32" />
+            </div>
+          </div>
+        </CardContent>
+      </Card>)
   } else {
     content = <div>Loading...</div>;
   }
