e87668995a2457771c50a396ed40aa115ce22f03
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-19 19:49:12 +0900

    Update image generator

--- app/api/users/[username]/opengraph-image/route.tsx
+++ app/api/users/[username]/opengraph-image/route.tsx
@@ -22,39 +22,22 @@ export async function GET(request: Request,
           return new ImageResponse(
                ( <div tw="flex flex-col w-full h-screen items-center justify-center bg-white">
                <div tw="bg-gray-50 flex w-full h-full">
-                 <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-                   <div tw="absolute top-10 left-10">
-                       <Icons.logo tw="h-10 w-72"/>
+               <div tw="absolute flex top-10 left-16">
+                     <Icons.logo tw="w-72 h-10" />
                    </div>
-                   <div tw="absolute bottom-10 left-10">
-                   <div tw="flex items-center">
-                       <span tw="rounded-full h-48 w-48">
-                        <Image tw="rounded-full" src={user?.profilepicture} alt={user?.name} />
-
-                        </span>
-                     {
+                   <div tw="absolute bottom-10 left-10 flex flex-col">
+                   {
                            user?.name === null ? (
                              <h1 tw="space-y-3">
                          <span tw="font-bold text-[48px] block ">{user?.username}</span>
                        </h1>
                            ) : (
-                             <h1 tw="space-y-3">
-                         <span tw="font-bold text-[48px] block">{user?.name}</span>
-                         <span tw="text-[30px] font-light text-muted-foreground">{user?.username}</span>
-                       </h1>)
+                             <>
+                         <h1 tw="font-bold text-[48px] block">{user?.name}</h1>
+                       <span tw="text-[30px] font-light text-muted-foreground">{user?.username}</span>
+                             </>)
                          }
                    </div>
-                   <div tw="mt-5 grid gap-6 grid-cols-3">
-                         <div tw="text-xl bg-secondary">
-                           {user?.followersnum} Followers
-                         </div>
-                         <div tw="text-xl bg-secondary">
-                           {user?.followingnum} Followers
-                         </div>
-                         <div tw="text-xl bg-secondary" >{user?.postsnum} Post</div>
-                       </div>
-                   </div>
-                 </div>
                </div>
              </div>
                ),
