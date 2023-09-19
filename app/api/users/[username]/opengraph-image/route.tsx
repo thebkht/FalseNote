c1f25490c52308b69569191050736d1658c8122d
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-19 19:52:24 +0900

    test

--- app/api/users/[username]/opengraph-image/route.tsx
+++ app/api/users/[username]/opengraph-image/route.tsx
@@ -19,33 +19,33 @@ export async function GET(request: Request,
           const data = await response.json();  
            const user = data.user;
 
-          return new ImageResponse(
-               ( <div tw="flex flex-col w-full h-screen items-center justify-center bg-white">
-               <div tw="bg-gray-50 flex w-full h-full">
-               <div tw="absolute flex top-10 left-16">
-                     <Icons.logo tw="w-72 h-10" />
-                   </div>
-                   <div tw="absolute bottom-10 left-10 flex flex-col">
-                   {
-                           user?.name === null ? (
-                             <h1 tw="space-y-3">
-                         <span tw="font-bold text-[48px] block ">{user?.username}</span>
-                       </h1>
-                           ) : (
-                             <>
-                         <h1 tw="font-bold text-[48px] block">{user?.name}</h1>
-                       <span tw="text-[30px] font-light text-muted-foreground">{user?.username}</span>
-                             </>)
-                         }
-                   </div>
-               </div>
-             </div>
-               ),
-               {
-                 width: 1200,
-                 height: 630,
-               },
-             );
+           return new ImageResponse(
+            ( <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
+            <div tw="flex w-full h-full">
+              <div tw="absolute flex top-10 left-16">
+                  <Icons.logo tw="w-72 h-10" />
+                </div>
+                <div tw="absolute bottom-10 left-10 flex flex-col">
+                {
+           user?.name === null ? (
+             <h1 tw="space-y-3">
+         <span tw="font-bold text-2xl block mb-2">{user?.username}</span>
+       </h1>
+           ) : (
+             <h1 tw="space-y-3 inline-block">
+         <span tw="font-bold text-[40px] block mb-2">{user?.name}</span>
+         <span tw="text-2xl font-light text-muted-foreground">{user?.username}</span>
+       </h1>)
+         }
+                </div>
+            </div>
+            </div>
+            ),
+            {
+              width: 1200,
+              height: 630,
+            },
+          );
      }
      catch (e: any) {
           console.log(`${e.message}`);
