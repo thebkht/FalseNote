9bb96b70d03ddc4f79bcaea58fd17ec969dbed89
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-20 17:29:02 +0900

    Update landing page

--- components/landing/landing.tsx
+++ components/landing/landing.tsx
@@ -10,8 +10,8 @@ export default function Landing() {
           <div className="landing_hero-image my-20">
             <Image src={headerImage} alt="" />
           </div>
-          <h1 className="landing__hero_title max-w-[1200px] font-black leading-snug text-8xl text-center">Start Your Journey with FalseNotes!</h1>
-          <p className="landing__hero_description mt-6 mx-auto max-w-[960px] font-medium text-3xl leading-relaxed text-center">🚀 FalseNotes — a platform for Developers to Spark Discussions, Share Expertise, and Shape Coding Journeys.</p>
+          <h1 className="landing__hero_title max-w-[1200px] font-black leading-snug text-7xl text-center">Start Your Journey with FalseNotes!</h1>
+          <p className="landing__hero_description mt-6 mx-auto max-w-[960px] font-medium text-2xl leading-relaxed text-center">🚀 FalseNotes — a platform for Developers to Spark Discussions, Share Expertise, and Shape Coding Journeys.</p>
         </div>
       </div> 
       </main> 
