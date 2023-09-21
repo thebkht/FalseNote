653a553bd267f85d0618f4b1917924c4c78b8d82
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-21 16:17:01 +0900

    Update post settings

--- components/editor/post-form.tsx
+++ components/editor/post-form.tsx
@@ -33,11 +33,14 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
+import Image from "next/image"
+import { AspectRatio } from "../ui/aspect-ratio"
 
 const postFormSchema = z.object({
   title: z
@@ -59,7 +62,8 @@ const postFormSchema = z.object({
       })
     )
     .optional(),
-  url : z.string(),
+  url: z.string(),
+  coverImage: z.string().optional(),
 })
 
 type PostFormValues = z.infer<typeof postFormSchema>
@@ -67,6 +71,7 @@ type PostFormValues = z.infer<typeof postFormSchema>
 // This can come from your database or API.
 const defaultValues: Partial<PostFormValues> = {
   visibility: "public",
+  url: "",
 }
 
 export function PostForm() {
@@ -88,11 +93,11 @@ export function PostForm() {
       description: (
         <div>
           <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-        <div className="mt-4">
-        <ReactMarkdown>{markdownContent}</ReactMarkdown>
-      </div>
+            <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+          </pre>
+          <div className="mt-4">
+            <ReactMarkdown>{markdownContent}</ReactMarkdown>
+          </div>
         </div>
       ),
     })
@@ -100,10 +105,20 @@ export function PostForm() {
 
 
   const [markdownContent, setMarkdownContent] = useState<string>(''); // State for Markdown content
+  const [coverImg, setCoverImg] = useState<string>('');
 
   function handleContentChange(value: string) {
     setMarkdownContent(value);
     form.setValue('content', value); // Update the form field value
+  }
+
+  function handleCoverImgChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file); // Create a URL from the selected file
+      setCoverImg(imageUrl); // Set the URL as the cover image
+      form.setValue('coverImage', imageUrl); // Update the form field value with the URL
+    }
   }
 
   return (
@@ -150,102 +165,143 @@ export function PostForm() {
           )}
         />
         <Dialog>
-  <DialogTrigger><Button size={"lg"} className="w-full">Submit</Button></DialogTrigger>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Post Settings for publishing</DialogTitle>
-      <DialogDescription>
-      <FormField
-          control={form.control}
-          name="visibility"
-          render={({ field }) => (
-            <FormItem className="space-y-3">
-              <FormLabel>Privacy</FormLabel>
-              <FormControl>
-                <RadioGroup
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                  className="flex flex-col space-y-1"
-                >
-                  <FormItem className="flex items-center space-x-3 space-y-0">
-                    <FormControl>
-                      <RadioGroupItem value="public" />
-                    </FormControl>
-                    <FormLabel className="font-normal">
-                      Public
-                    </FormLabel>
-                  </FormItem>
-                  <FormItem className="flex items-center space-x-3 space-y-0">
-                    <FormControl>
-                      <RadioGroupItem value="private" />
-                    </FormControl>
-                    <FormLabel className="font-normal">
-                      Private
-                    </FormLabel>
-                  </FormItem>
-                  <FormItem className="flex items-center space-x-3 space-y-0">
-                    <FormControl>
-                      <RadioGroupItem value="draft" />
-                    </FormControl>
-                    <FormLabel className="font-normal">Draft</FormLabel>
-                  </FormItem>
-                </RadioGroup>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="url"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>URL-friendly Link</FormLabel>
-              <FormDescription>
-                teletype.in/@bkhtdev/
-              </FormDescription>
-              <FormControl>
-                <Input placeholder="URL" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="grid gap-4 grid-cols-5">
-          {fields.map((field, index) => (
-            <FormField
-              control={form.control}
-              key={field.id}
-              name={`topics.${index}.value`}
-              render={({ field }) => (
-                <FormItem>
-                  {/* <FormDescription className={cn(index !== 0 && "sr-only")}>
+          <DialogTrigger><Button size={"lg"} className="w-full">Submit</Button></DialogTrigger>
+          <DialogContent>
+            <DialogHeader className="space-y-4">
+              <DialogTitle>Post Settings for publishing</DialogTitle>
+              <div className="mt-10 space-y-4">
+                <FormField
+                  control={form.control}
+                  name="visibility"
+                  render={({ field }) => (
+                    <FormItem className="space-y-3">
+                      <FormLabel>Privacy</FormLabel>
+                      <FormControl>
+                        <RadioGroup
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                          className="flex flex-col space-y-1"
+                        >
+                          <FormItem className="flex items-center space-x-3 space-y-0">
+                            <FormControl>
+                              <RadioGroupItem value="public" />
+                            </FormControl>
+                            <FormLabel className="font-normal">
+                              Public
+                            </FormLabel>
+                          </FormItem>
+                          <FormItem className="flex items-center space-x-3 space-y-0">
+                            <FormControl>
+                              <RadioGroupItem value="private" />
+                            </FormControl>
+                            <FormLabel className="font-normal">
+                              Private
+                            </FormLabel>
+                          </FormItem>
+                          <FormItem className="flex items-center space-x-3 space-y-0">
+                            <FormControl>
+                              <RadioGroupItem value="draft" />
+                            </FormControl>
+                            <FormLabel className="font-normal">Draft</FormLabel>
+                          </FormItem>
+                        </RadioGroup>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="url"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>URL-friendly Link</FormLabel>
+                      <FormDescription>
+                        teletype.in/@bkhtdev/
+                      </FormDescription>
+                      <FormControl>
+                        <Input placeholder="URL" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="coverImage"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Post Preview</FormLabel>
+                      <FormControl>
+                        <>
+                        {
+                          coverImg !== '' ?   (
+                            <AspectRatio ratio={16 / 9} className="bg-muted">
+                              <Image
+                                src={coverImg}
+                                alt="Photo by Drew Beamer"
+                                fill
+                                className="rounded-md object-cover"
+                              />
+                            </AspectRatio>
+                          ) : ''
+                        }
+                        <Input type="file" {...field} accept="image/*" onChange={handleCoverImgChange} />
+
+                        </>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <div className="grid gap-4 grid-cols-5">
+                  {fields.map((field, index) => (
+                    <FormField
+                      control={form.control}
+                      key={field.id}
+                      name={`topics.${index}.value`}
+                      render={({ field }) => (
+                        <FormItem>
+                          {/* <FormDescription className={cn(index !== 0 && "sr-only")}>
                     Add links to your website, blog, or social media profiles.
                   </FormDescription> */}
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          ))}
-          <Button
-            type="button"
-            variant="outline"
-            size="sm"
-            className="mt-2 col-span-5"
-            onClick={() => append({ value: "" })}
-          >
-            Add Topic
-          </Button>
-        </div>
-      <Button size={"lg"} className="w-full" type="submit">Submit</Button>
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
-        
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  ))}
+                  <Button
+                    variant="outline"
+                    className="mt-2 col-span-5"
+                    onClick={() => append({ value: "" })}
+                  >
+                    Add Topic
+                  </Button>
+                </div>
+              </div>
+              <DialogFooter className="mt-4">
+                <Button size={"lg"} className="w-full" type="submit">
+                  Publish
+                </Button>
+              </DialogFooter>
+            </DialogHeader>
+            {/* {
+      markdownContent && (
+        <DialogDescription>
+      <div className="">
+        <ReactMarkdown>{markdownContent}</ReactMarkdown>
+      </div>
+    </DialogDescription>
+      )
+    } */}
+          </DialogContent>
+        </Dialog>
+
       </form>
     </Form>
   )
--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     images: {
-          domains: ["avatars.githubusercontent.com"],
-        },
+
 }
 
 module.exports = {
