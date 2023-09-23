c66f147bfdf3d8001891b3be0a8c61ee14fb129a
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-24 04:05:19 +0900

    Update next config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
               port: '',
               pathname: `/**`,
             },
+            {
+              protocol: 'https',
+              hostname: `s3.ap-northeast-2.amazonaws.com`,
+              port: '',
+              pathname: `/falsenotes.app/**`,
+            }
           ],
         },
 }
