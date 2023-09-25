1a2fcb83c4d22b2eed6004e5cddd42a2b043805f
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-25 18:07:46 +0900

    Update feed

--- app/(Main)/feed/page.tsx
+++ app/(Main)/feed/page.tsx
@@ -1,6 +1,12 @@
+import dynamic from 'next/dynamic';
+
 export default function Feed() {
+     const LoadedFeed = dynamic(() => import('@/components/feed/feed'), {
+          ssr: false,
+     });
      return (
           <>
+               <LoadedFeed />
           </>
      )
 }
