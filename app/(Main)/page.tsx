0e380724cb4223ff40dd77ef9649c09c960a4312
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-25 17:16:40 +0900

    Init lazy load

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -3,13 +3,21 @@ import Landing from '@/components/landing/landing'
 import { useSession } from 'next-auth/react'
 import Feed from '@/components/feed/feed'
 import { use, useEffect } from 'react';
+import dynamic from 'next/dynamic';
 
 export default function Home() {
   const { status, update } = useSession();
 
+  const LoadedFeed = dynamic(() => import('@/components/feed/feed'), {
+    ssr: false,
+  });
+  const LoadedLanding = dynamic(() => import('@/components/landing/landing'), {
+    ssr: false,
+  });
+
   if (status !== "authenticated") {
-    return <Landing />
+    return <LoadedLanding />
   } else {
-    return <Feed />
+    return <LoadedFeed />
   }
 }
