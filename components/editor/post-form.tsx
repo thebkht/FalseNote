1ef33516d2b757dab3b62a4e6c7b30074ee814d1
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-27 03:10:00 +0900

    Fix description onchange issue

--- components/editor/post-form.tsx
+++ components/editor/post-form.tsx
@@ -390,7 +390,8 @@ export function PostForm() {
                     <FormItem>
                       <FormLabel>Post Description</FormLabel>
                       <FormControl>
-                        <TextareaAutosize {...field} className="flex rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-full min-h-[40px]" rows={1}/>
+                        <TextareaAutosize {...field} className="flex rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-full min-h-[40px]" rows={1}
+                        onChange={field.onChange}/>
                       </FormControl>
                       <FormMessage />
                     </FormItem>
