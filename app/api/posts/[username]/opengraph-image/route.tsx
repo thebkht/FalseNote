85413878d45520eb6f2cde85fb625308eaeb7ca9
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-27 07:32:30 +0900

    Test

--- app/api/posts/[username]/opengraph-image/route.tsx
+++ app/api/posts/[username]/opengraph-image/route.tsx
@@ -15,10 +15,11 @@ const formatDate = (dateString: string | number | Date) => {
      })
      return formattedDate
 }
-export async function middleware(req: NextRequest, { params }: { params: { username: string } }) {
+export async function GET(req: Request, { params }: { params: { username: string } }) {
      try {
           const { username } = params
-     const postUrl = req.nextUrl.searchParams.get("url");
+     const url = new URL(req.url);
+     const postUrl = url.searchParams.get("url");
 
      const response = await fetch(`${process.env.DOMAIN}/api/posts/${username}?url=${postUrl}`);
      if (!response.ok) {
