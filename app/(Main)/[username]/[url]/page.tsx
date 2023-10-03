bc736bdd4f86db5137c6d6194edb3b4d1cf37ca8
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-10-03 20:15:51 +0900

    Fix issue

--- app/(Main)/[username]/[url]/page.tsx
+++ app/(Main)/[username]/[url]/page.tsx
@@ -21,7 +21,7 @@ import { useRouter } from "next/router"
 import Image from "next/image"
 import { AspectRatio } from "@/components/ui/aspect-ratio"
 
-export const formatDate = (dateString: string | number | Date) => {
+const formatDate = (dateString: string | number | Date) => {
      const date = new Date(dateString)
      const currentYear = new Date().getFullYear()
      const year = date.getFullYear()
