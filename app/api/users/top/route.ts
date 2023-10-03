4f71fc42afa374b7aeec3d1e556ed0533b2ea433
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-10-03 19:02:43 +0900

    Update top users

--- app/api/users/top/route.ts
+++ app/api/users/top/route.ts
@@ -2,27 +2,25 @@ import { sql } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
 
 // api to execute the top users query by followers and return the result
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   try {
-    // Execute a query to fetch all users
-    const users = await sql`SELECT * FROM Users`;
+    const userid = request.nextUrl.searchParams.get("user");
 
-    // Create an array of promises for fetching followers
-    const followersPromises = users.rows.map(async (user: any) => {
-      const followers = await sql`SELECT * FROM Follows WHERE FolloweeID = ${user.userid}`;
-      user.followers = followers.rows;
-    });
+    // execute the query to fetch the top 5 users by followers where the user is not following them and dont display the user itself
 
-    // Wait for all follower queries to complete
-    await Promise.all(followersPromises);
-
-    // Sort the users by followers
-    users.rows.sort((a: any, b: any) => {
-      return b.followers.length - a.followers.length;
-    });
-
-    // Return the top 10 users
-    return NextResponse.json(users.rows.slice(0, 10));
+    const { rows: users } = await sql`
+    SELECT *
+    FROM Users
+    WHERE userid NOT IN (
+      SELECT followeeid
+      FROM Follows
+      WHERE followerid = ${userid}
+    ) AND userid != ${userid}
+    ORDER BY followers DESC
+    LIMIT 5
+  `;
+    // return the result
+    return NextResponse.json({ users });
   } catch (error) {
     return NextResponse.json({ error: "Error fetching users" }, { status: 500 });
   }
--- components/feed/feed.tsx
+++ components/feed/feed.tsx
@@ -4,22 +4,28 @@ import { Input } from "@/components/ui/input";
 import FeaturedDev from "./featured/featured-dev";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
+import { get } from "http";
+import { getSessionUser } from "../get-session-user";
 
 export default function EmptyFeed() {
   const [featuredDevs, setFeaturedDevs] = useState([]);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const { status, data: session } = useSession();
 
   useEffect(() => {
     async function fetchData() {
-      try {
-        const userData = await fetch(`/api/users/top`, {
-          method: "GET",
-          }).then((res) => res.json());
-        setFeaturedDevs(userData);
-        setIsLoaded(true);
-      } catch (error) {
-        // Handle errors
-        console.error('Error:', error);
+      if (status !== "unauthenticated") {
+        try {
+          const userid = (await getSessionUser()).userid;
+          const userData = await fetch(`/api/users/top?user=${userid}`, {
+            method: "GET",
+            }).then((res) => res.json());
+          setFeaturedDevs(userData);
+          setIsLoaded(true);
+        } catch (error) {
+          // Handle errors
+          console.error('Error:', error);
+        }
       }
     }
 
