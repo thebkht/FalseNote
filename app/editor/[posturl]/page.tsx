aacf6be0d61545b87dbc59dcfb153a811241dcc0
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-10-04 03:24:27 +0900

    test

--- app/editor/[posturl]/page.tsx
+++ app/editor/[posturl]/page.tsx
@@ -1,6 +1,6 @@
 import { PostEditorForm } from '@/components/editor/post-editor-form'
 
-export default function PostEditor(params: { posturl: string }) {
+export default function PostEditor({ params }: { params: { posturl: string } }) {
   return (
     
     <main className="flex min-h-screen flex-col items-center justify-between px-6 py-14 lg:p-20 editor">
