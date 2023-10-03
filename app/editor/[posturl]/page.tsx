131a2afaf13b29cf8745c3b885b3554065b6949f
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-10-04 04:10:53 +0900

    Fix issue

--- app/editor/[posturl]/page.tsx
+++ app/editor/[posturl]/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 import { PostEditorForm } from '@/components/editor/post-editor-form'
 import { getSessionUser } from '@/components/get-session-user';
+import { redirect } from 'next/navigation';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 export default function PostEditor({ params }: { params: { posturl: string } }) {
   const [user, setUser] = useState<any | null>(null);
   const [post, setPost] = useState<any>(null); // State for the post object
-  const router = useRouter();
 
   useEffect(() => {
     async function fetchData() {
@@ -21,12 +21,12 @@ export default function PostEditor({ params }: { params: { posturl: string } }) 
         setPost(post);
       } catch (error) {
         console.error(error);
-        router.push('/404');
+        redirect("/404");
       }
     }
 
     fetchData();
-  }, [params.posturl, router]);
+  }, [params.posturl]);
   return (
     
     <main className="flex min-h-screen flex-col items-center justify-between px-6 py-14 lg:p-20 editor">
