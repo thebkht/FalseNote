dbe0f737c86e17f6b6972f9dc35aff3863ddf03e
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-10-04 18:40:53 +0900

    Update

--- app/api/comments/create/route.ts
+++ app/api/comments/create/route.ts
@@ -1,7 +1,7 @@
 import { sql } from "@vercel/postgres";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function POST(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const { post, content, author } = req.body;
 
