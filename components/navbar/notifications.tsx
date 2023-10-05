2d387b518916010c68e883b57c37378c33639f6a
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-10-06 01:37:39 +0900

    Update notifications

--- components/navbar/notifications.tsx
+++ components/navbar/notifications.tsx
@@ -10,6 +10,54 @@ import { Button } from "../ui/button"
 import { Bell } from "lucide-react"
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
 
+function dateFormat(dateString: string | number | Date) {
+     const date = new Date(dateString)
+     const currentDate = new Date()
+     const currentYear = currentDate.getFullYear()
+     const currentDay = currentDate.getDate()
+     const currentHour = currentDate.getHours()
+     const currentMinute = currentDate.getMinutes()
+     const currentSecond = currentDate.getSeconds()
+   
+     const year = date.getFullYear()
+     const month = date.getMonth()
+     const day = date.getDate()
+     const hour = date.getHours()
+     const minute = date.getMinutes()
+     const second = date.getSeconds()
+   
+     const dayDifference = currentDay - day
+     const hourDifference = currentHour - hour
+     const minuteDifference = currentMinute - minute
+     const secondDifference = currentSecond - second
+   
+     //when posted ex: 1 hour ago 1 day ago
+     if (dayDifference === 0) {
+           if (hourDifference === 0) {
+                 if (minuteDifference === 0) {
+                     return `${secondDifference}s`
+                 }
+                 return `${minuteDifference}m`
+           }
+           return `${hourDifference}h`
+       } 
+       //if more than 30 days ago, show date ex: Apr 4, 2021
+       if (dayDifference > 30) {
+           if (year !== currentYear) {
+               return `${date.toLocaleDateString('en-US', {
+                   year: 'numeric',
+                   month: 'short',
+                   day: 'numeric',
+               })}`
+           }
+           return `${date.toLocaleDateString('en-US', {
+               month: 'short',
+               day: 'numeric',
+           })}`
+       } else {
+         return `${dayDifference}d`
+       }
+   }
 
 export function Notifications({ notifications, className, ...props }: React.ComponentPropsWithoutRef<typeof DropdownMenu> & { className?: string, notifications: any }) {
      return (
@@ -18,14 +66,13 @@ export function Notifications({ notifications, className, ...props }: React.Comp
                     <DropdownMenuTrigger><Button variant={"ghost"} size={"icon"}>
                     <Bell className="w-[1.25rem] h-[1.25rem]" />
                </Button></DropdownMenuTrigger>
-                    <DropdownMenuContent>
+                    <DropdownMenuContent className="max-w-96" align="end" forceMount>
                          <DropdownMenuLabel>Notifications</DropdownMenuLabel>
                          <DropdownMenuSeparator />
                          {notifications.map((notification: any) => (
                               <DropdownMenuItem key={notification.id}>
-                                   <div className="flex items-center justify-between w-full">
-                                        <div className="flex items-center gap-3">
-                                             {
+                                   <div className="flex items-center justify-between w-full gap-3">
+                                   {
                                                   notification.sender && (
                                                        <Avatar>
                                                             <AvatarImage src={notification.sender.profilepicture} alt={notification.sender.username} />
@@ -33,11 +80,12 @@ export function Notifications({ notifications, className, ...props }: React.Comp
                                                        </Avatar>
                                                   )
                                              }
-                                             <div className="flex flex-col">
-                                                  <span className="text-sm text-muted-foreground">{notification.message}</span>
+                                        <div className="flex flex-col gap-y-2">
+                                                  <span className="text-sm">{
+                                                       notification.message.lenght > 50 ? notification.message.substring(0, 50) + "..." : notification.message
+                                                  }</span>
+                                                  <span className="text-xs text-muted-foreground">{dateFormat(notification.createdat)}</span>
                                              </div>
-                                        </div>
-                                        <span className="text-xs text-muted-foreground">{notification.createdat}</span>
                                    </div>
                               </DropdownMenuItem>
                          ))}
