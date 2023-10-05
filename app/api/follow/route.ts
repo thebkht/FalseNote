2f44271607c08160011325827bdd24c0ae20e80a
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-10-06 02:42:56 +0900

    Add follow notification

--- app/api/follow/route.ts
+++ app/api/follow/route.ts
@@ -16,6 +16,21 @@ export async function GET(request: NextRequest) {
           await sql`DELETE FROM follows WHERE followeeId = ${followeeId} AND followerId = ${followerId}`;
      } else {
           await sql`INSERT INTO follows (followeeId, followerId) VALUES (${followeeId}, ${followerId})`;
+
+          const { rows: followeeDetails } = await sql`SELECT * FROM users WHERE userid = ${followeeId}`;
+          const { rows: followerDetails } = await sql`SELECT * FROM users WHERE userid = ${followerId}`;
+
+          const message = `${followerDetails[0].username} is now following you`;
+          const user_id = followeeId;
+          const type = "follow";
+          const created_at = new Date().toISOString();
+          const read_at = null;
+          const sender_id = followerId;
+
+          await sql`
+            INSERT INTO notifications (type, message, userid, createdat, readat, sender_id)
+            VALUES (${type}, ${message}, ${user_id}, ${created_at}, ${read_at}, ${sender_id})
+          `;
      }
 
      return new Response("followed", { status: 200 });
