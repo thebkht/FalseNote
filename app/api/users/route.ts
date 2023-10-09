fc290f024d76defcade38ddd365ec9ba10ec1b28
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-10-10 02:01:45 +0900

    Fix issue

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -16,19 +16,3 @@ export async function GET(request: Request) {
     return NextResponse.json({ error }, { status: 500 });
   }
 }
-
-export async function getUser(request: NextRequest) {
-  try {
-    // Execute a query to fetch all table names
-    const result = await sql`
-      SELECT * FROM users WHERE Username = ${request.nextUrl.searchParams.get('username')}
-    `;
-
-    const users = result.rows;
-
-    console.log(users);
-    return NextResponse.json({ users }, { status: 200});
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
-  }
-}
