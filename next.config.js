d70d29d6c71a2cc8060875071e030580c1682c08
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-10-20 00:20:41 +0900

    test

--- next.config.js
+++ next.config.js
@@ -22,6 +22,13 @@ const nextConfig = {
             }
           ],
         },
+        webpack: (config, { webpack }) => {
+          config.plugins.push(new webpack.IgnorePlugin({
+              resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
+          }))
+  
+          return config
+      },
 }
 
 module.exports = {
