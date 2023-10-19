fe583d62c98307a0eabded35c2e1aa0b3c2a0b62
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-10-19 20:20:29 +0900

    test

--- lib/postgres.ts
+++ lib/postgres.ts
@@ -1,9 +1,19 @@
 import postgres from "postgres";
 
-export const sql = postgres(`${process.env.POSTGRES_URL}`, {
+export const sql = process.env.NODE_ENV === "production" ? postgres(`${process.env.POSTGRES_URL}`, {
      user: process.env.POSTGRES_USER,
      host: process.env.POSTGRES_HOST,
      database: process.env.POSTGRES_DATABASE,
      password: process.env.POSTGRES_PASSWORD,
      port: 5432,
-})
+     ssl: {
+          rejectUnauthorized: false,
+     },
+}) : postgres(`${process.env.POSTGRES_URL}`, {
+     user: process.env.POSTGRES_USER,
+     host: process.env.POSTGRES_HOST,
+     database: process.env.POSTGRES_DATABASE,
+     password: process.env.POSTGRES_PASSWORD,
+     port: 5432,
+     
+});
