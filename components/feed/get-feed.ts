46aa56bdfa21a5a8652bb8f223533baf47ab29d3
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-10-31 07:18:46 +0900

    update

--- components/feed/get-feed.ts
+++ components/feed/get-feed.ts
@@ -5,10 +5,23 @@ import { getSessionUser } from "../get-session-user";
 export const fetchFeed = async ({ page = 0, tag }: { page?: number, tag?: string | undefined }) => {
       const user = await getSessionUser();
 
-      const result = await fetch(process.env.DOMAIN + "/api/feed?page=" + page + "?tag=" + "?id=" + user?.id , {
-            method: "GET",
-            },
-            ).then((res) => res.json());
-      
-      return result?.feed;
+      if (!user) {
+            return null;
+      } else {
+            if (tag) {
+                  const result = await fetch(process.env.DOMAIN + "/api/feed?page=" + page + "?tag=" + "?id=" + user?.id , {
+                        method: "GET",
+                        },
+                        ).then((res) => res.json());
+                  
+                  return result?.feed;
+            } else {
+                  const result = await fetch(process.env.DOMAIN + "/api/feed?page=" + page + "?id=" + user?.id , {
+                        method: "GET",
+                        },
+                        ).then((res) => res.json());
+                  
+                  return result?.feed;
+            }
+      }
 }
