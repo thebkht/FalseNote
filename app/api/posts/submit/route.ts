0fe798ab7d14e56cbfbe7c9d9f0b3828adbb9368
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-02 20:46:01 +0900

    Update post editor

--- app/api/posts/submit/route.ts
+++ app/api/posts/submit/route.ts
@@ -89,6 +89,7 @@ export async function POST(req: NextRequest) {
                               url: url,
                               subtitle: subtitle,
                               readingTime: readTime,
+                              updated: true,
                          }
                     })
 
