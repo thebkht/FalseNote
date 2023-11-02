159ef719b4c9937d01d5c3b1c28ba14ccd07ccfd
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-02 17:21:46 +0900

    update search bar

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -1,4 +1,4 @@
-import { Search } from "lucide-react";
+import { Hash, Search } from "lucide-react";
 import {
   Command,
   CommandDialog,
@@ -14,30 +14,45 @@ import React, { useEffect, useRef, useState } from "react";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
 import { useDebounce } from "use-debounce";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
+// This is a placeholder function. Replace it with your actual implementation.
+async function fetchSuggestions(query: string) {
+  const [userResponse, tagResponse] = await Promise.all([
+    fetch(`/api/users?query=${encodeURIComponent(query)}&limit=3`),
+    fetch(`/api/tags?query=${encodeURIComponent(query)}&limit=3`),
+  ]);
 
+  if (!userResponse.ok) {
+    throw new Error(`User API request failed: ${userResponse.status}`);
+  }
+  if (!tagResponse.ok) {
+    throw new Error(`Tag API request failed: ${tagResponse.status}`);
+  }
+
+  const users = await userResponse.json();
+  const tags = await tagResponse.json();
+
+  return { users, tags };
+}
 
 export default function SearchBar() {
   const [open, setOpen] = React.useState(false)
   const router = useRouter()
-     const initialRender = useRef(true)
+  const initialRender = useRef(true)
    
-     const [text, setText] = useState(null)
-     const [query] = useDebounce(text, 750)
-   
-     useEffect(() => {
-       if (initialRender.current) {
-         initialRender.current = false
-         return
-       }
-   
-       if (!query) {
-         router.push(`/explore`)
-       } else {
-         router.push(`/explore?search=${query}`)
-       }
-     }, [query])
- 
+  const [text, setText] = useState(null)
+  const [query] = useDebounce(text, 750)
+  const [suggestions, setSuggestions] = useState<any>([])
+
+  useEffect(() => {
+    if (query) {
+      fetchSuggestions(query).then((result) => setSuggestions(result));
+    } else {
+      setSuggestions({ users: [], tags: [] });
+    }
+  }, [query]);
+
   React.useEffect(() => {
     const down = (e: KeyboardEvent) => {
       if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
@@ -52,12 +67,55 @@ export default function SearchBar() {
   return (
     <>
       <Button variant={"ghost"} size={"icon"} onClick={() => setOpen((open) => !open)} ><Search className="h-[1.2rem] w-[1.2rem]"/></Button>
-    <CommandDialog open={open} onOpenChange={setOpen}>
-      <CommandInput placeholder="Search for people or tags..." onSubmit={(event: FormEvent<HTMLInputElement>) => { event.preventDefault(); router.push(`/explore?search=${text}`); }} onValueChange={setText} />
-      <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-      </CommandList>
-    </CommandDialog>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search for people or tags..." />
+        <CommandList>
+          {
+            suggestions.length === 0 && (
+              <CommandEmpty>
+                <p>No results found</p>
+              </CommandEmpty>
+            )
+          }
+          {
+            //users
+            suggestions?.users?.length > 0 && (
+              <CommandGroup title="Users">
+                {suggestions.users.map((user: any) => (
+                  <CommandItem
+                    key={user.id}
+                    onClick={() => {
+                      router.push(`/${user.username}`)
+                    }}
+                  >
+                    <Avatar>
+                      <AvatarImage src={user.avatar} alt={user.username} />
+                      <AvatarFallback>{user.username}</AvatarFallback>
+                    </Avatar>
+                    <span>{user.name || user.username}</span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            )}{suggestions?.users?.length > 0 && suggestions?.tags?.length > 0 && <CommandSeparator />}{
+            //tags
+            suggestions?.tags?.length > 0 && (
+              <CommandGroup title="Tags">
+                {suggestions.tags.map((tag: any) => (
+                  <CommandItem
+                    key={tag.id}
+                    onClick={() => {
+                      router.push(`/tags/${tag.name}`)
+                    }}
+                  >
+                    <Hash className="mr-2 h-4 w-4" />
+                    <span>{tag.name}</span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            )            
+          }
+        </CommandList>
+      </CommandDialog>
     </>
   )
 }
