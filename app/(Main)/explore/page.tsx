c532bfbcae4cf8f52ee83effdfebb2809751c276
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-06 20:23:26 +0900

    Add empty placeholder for no search results in Explore page

--- app/(Main)/explore/page.tsx
+++ app/(Main)/explore/page.tsx
@@ -1,3 +1,4 @@
+import { EmptyPlaceholder } from "@/components/empty-placeholder"
 import Posts from "@/components/explore/posts"
 import Search from "@/components/explore/search"
 import { getSessionUser } from "@/components/get-session-user"
@@ -13,21 +14,28 @@ export default async function Explore({
      const search = typeof searchParams.search === 'string' ? searchParams.search : undefined
 
      const { posts } = await getPosts({ search, limit: 5 })
+     
      const session = await getSessionUser()
      return (
           <>
                <div className="flex flex-col items-center pt-10">
                     <h2 className="font-medium text-4xl">Explore</h2>
                     <Search search={search} />
+                    <div className="w-2/3 mb-10">
                     {posts.length > 0 && <Posts initialPosts={posts} search={search} session={session} />}
                     {posts.length === 0 && search && (
-                         <div className="w-full max-h-screen my-auto flex justify-center items-center bg-background">
-                              <div className="flex flex-col items-center justify-center space-y-4">
-                                   <h1 className="text-2xl font-bold">No posts found</h1>
-                                   <p className="text-muted-foreground">Try searching for something else.</p>
-                              </div>
-                         </div>)
+                         <div className="flex flex-col items-center justify-center w-full">
+                              <EmptyPlaceholder>
+                              <EmptyPlaceholder.Icon name="post" strokeWidth={1.25} />
+                              <EmptyPlaceholder.Title>No posts found</EmptyPlaceholder.Title>
+                              <EmptyPlaceholder.Description>
+                                   Try searching for something else.
+                              </EmptyPlaceholder.Description>
+                         </EmptyPlaceholder>
+                         </div>
+                         )
                     }
+                    </div>
                </div>
           </>
      )
