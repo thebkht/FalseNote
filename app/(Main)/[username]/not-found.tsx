ae3d6af68ee29243063fc1edf78a78bbc2ebbd7e
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-08 01:15:57 +0900

    Add alert dialog to not-found page

--- app/(Main)/[username]/not-found.tsx
+++ app/(Main)/[username]/not-found.tsx
@@ -2,18 +2,23 @@ import Link from "next/link"
 
 import { buttonVariants } from "@/components/ui/button"
 import { EmptyPlaceholder } from "@/components/empty-placeholder"
+import { AlertDialog, AlertDialogContent } from "@/components/ui/alert-dialog"
 
 export default function NotFound() {
   return (
-    <EmptyPlaceholder className="mx-auto max-w-[800px]">
-      <EmptyPlaceholder.Icon name="warning" />
-      <EmptyPlaceholder.Title>Uh oh! Not Found</EmptyPlaceholder.Title>
-      <EmptyPlaceholder.Description>
-        This user cound not be found. Please try again.
-      </EmptyPlaceholder.Description>
-      <Link href="/" className={buttonVariants({ variant: "ghost" })}>
-        Go to Home
-      </Link>
-    </EmptyPlaceholder>
+    <AlertDialog open>
+      <AlertDialogContent className="p-0 border-none">
+        <EmptyPlaceholder className="w-full">
+          <EmptyPlaceholder.Icon name="warning" />
+          <EmptyPlaceholder.Title>Uh oh! Not Found</EmptyPlaceholder.Title>
+          <EmptyPlaceholder.Description>
+            This user cound not be found. Please try again.
+          </EmptyPlaceholder.Description>
+          <Link href="/" className={buttonVariants({ variant: "ghost" })}>
+            Go to Home
+          </Link>
+        </EmptyPlaceholder>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
