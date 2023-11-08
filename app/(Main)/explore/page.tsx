4a40be624f35195aea88ceaec360c79bbe62f9ac
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-08 23:36:23 +0900

    Refactor code to improve performance and readability.

--- app/(Main)/explore/page.tsx
+++ app/(Main)/explore/page.tsx
@@ -16,6 +16,8 @@ import { Separator } from "@/components/ui/separator"
 import { Button } from "@/components/ui/button"
 import UserHoverCard from "@/components/user-hover-card"
 import { searchTags } from "@/lib/prisma/tags"
+import { Badge } from "@/components/ui/badge"
+import ExploreComponent from "@/components/explore/tab-content"
 
 export default async function Explore({
      searchParams
@@ -33,122 +35,48 @@ export default async function Explore({
 
      const activeTab = tab || 'top'
 
-     const { posts } = await getPosts({ search, limit: 3 })
-     const { users } = await getUsers({ search, limit: 3 })
-     const { tags } = await searchTags({ search, limit: 3 })
-
+     let { posts } = await getPosts({ search, limit: 3 })
+     let { users } = await getUsers({ search, limit: 3 })
+     let { tags } = await searchTags({ search, limit: 3 })
 
      const session = await getSessionUser()
      // ...
 
      return (
           <>
-               <div className="flex flex-col items-center pt-10 space-y-8">
+               <div className="flex flex-col items-center py-10 space-y-8">
                     <h2 className="font-medium text-4xl">Explore</h2>
                     <Search search={search} />
-                    <Tabs defaultValue={activeTab} className="my-6">
-                         <TabsList className="bg-transparent gap-2">
+                    <Tabs defaultValue={activeTab} className="my-6 space-y-8">
+                         <TabsList className="bg-transparent gap-2 justify-center w-full">
                               <TabsTrigger value={'top'} className="bg-muted data-[state=active]:border data-[state=active]:border-foreground">
+                                   <Link href={`/explore${search !== undefined ? `?search=${search}` : ''}`}>
                                    Trending
+                                   </Link>
                               </TabsTrigger>
                               <TabsTrigger value={'posts'} className="bg-muted data-[state=active]:border data-[state=active]:border-foreground">
+                                   <Link href={`/explore?tab=posts${search !== undefined ? `&search=${search}` : ''}`}>
                                    Posts
+                                   </Link>
                               </TabsTrigger>
                               <TabsTrigger value={'users'} className="bg-muted data-[state=active]:border data-[state=active]:border-foreground">
+                                   <Link href={`/explore?tab=users${search !== undefined ? `&search=${search}` : ''}`}>
                                    Users
+                                   </Link>
                               </TabsTrigger>
                               <TabsTrigger value={'tags'} className="bg-muted data-[state=active]:border data-[state=active]:border-foreground">
+                                   <Link href={`/explore?tab=tags${search !== undefined ? `&search=${search}` : ''}`}>
                                    Tags
+                                   </Link>
                               </TabsTrigger>
                          </TabsList>
+                         <TabsContent value={'top'}>
+                              <ExploreComponent users={users} posts={posts} tags={tags} session={session} search={search} tab={tab} activeTab={activeTab} searchParams={searchParams} />
+                         </TabsContent>
                     </Tabs>
                     <div className="w-full mb-10 space-y-4">
-                         {posts.length > 0 && (
-                              <Card>
-                                   <CardHeader className="">
-                                        <CardTitle className="feed__content_featured_card_title text-base">Posts</CardTitle>
-                                   </CardHeader>
-                                   <CardContent className="">
-                                        <ol className="flex flex-col items-start justify-between space-y-4">
-                                             {posts.map(
-                                                  (item: any, index: number) => (
-                                                       <li key={item.id} className="text-sm space-y-2.5">
+                         
 
-                                                            <Link href={`/${item.author.username}`} className="text-xs flex items-center mb-2 font-medium">
-                                                                 <Avatar className="h-5 w-5 mr-1 md:mr-1.5 ">
-                                                                      <AvatarImage src={item.author?.image} alt={item.author?.username} />
-                                                                      <AvatarFallback>{item.author?.name?.charAt(0) || item.author?.username?.charAt(0)}</AvatarFallback>
-                                                                 </Avatar>
-                                                                 {item.author.name || item.author.username} {item.author?.verified && (
-                                                                      <Icons.verified className="h-3 w-3 mx-0.5 inline fill-primary align-middle" />
-                                                                 )}
-                                                            </Link>
-
-
-                                                            <Link href={`/${item.author.username}/${item.url}`} className="text-base font-bold line-clamp-2 overflow-hidden leading-tight">
-                                                                 {item.title}
-                                                            </Link>
-                                                       </li>
-                                                  ))}
-                                        </ol>
-                                   </CardContent>
-                                   <CardFooter className="flex justify-end">
-                                        <Link href={`/explore?tab=posts${search !== undefined ? `&search=${search}` : ''}`} className="flex items-center text-sm text-muted-foreground font-medium">
-                                             More
-                                        </Link>
-                                   </CardFooter>
-                              </Card>
-
-                         )}
-                         {
-                              users.length > 0 && (
-                                   <Card>
-                                        <CardHeader>
-                                             <CardTitle className="feed__content_featured_card_title text-base">Users</CardTitle>
-                                        </CardHeader>
-                                        <CardContent>
-                                             <div className="feed__content_featured_card_content flex flex-col items-start justify-between space-y-4">
-                                                  {users?.map(
-                                                       (item: any, index: number) => (
-                                                            <div className="flex gap-4 w-full items-center justify-between" key={item.id}>
-                                                                 <div className="space-y-3">
-                                                                      <UserHoverCard user={item} >
-                                                                           <Link href={`/${item.username}`} className="flex items-center">
-                                                                                <Avatar className="mr-1.5 md:mr-2 flex items-center justify-center bg-muted h-8 w-8">
-                                                                                     <AvatarImage src={item.image} alt={item.username} />
-                                                                                     <AvatarFallback>{item.name?.charAt(0) || item.username?.charAt(0)}</AvatarFallback>
-                                                                                </Avatar>
-                                                                                {
-                                                                                     !item.name ? (
-                                                                                          <div>
-                                                                                               <p className="text-sm font-medium leading-none">{item.username} {item?.verified && (
-                                                                                                    <Icons.verified className="h-3 w-3 mx-1 inline fill-primary align-middle" />
-                                                                                               )}</p>
-                                                                                          </div>
-                                                                                     ) : (
-                                                                                          <div>
-                                                                                               <p className="text-sm font-medium leading-none">{item.name} {item?.verified && (
-                                                                                                    <Icons.verified className="h-3 w-3 inline fill-primary align-middle" />
-                                                                                               )}</p>
-                                                                                               <p className="text-sm text-muted-foreground">{item.username}</p>
-                                                                                          </div>
-                                                                                     )
-                                                                                }
-                                                                           </Link>
-                                                                      </UserHoverCard>
-                                                                 </div>
-                                                            </div>
-                                                       ))}
-                                             </div>
-                                        </CardContent>
-                                        <CardFooter className="flex justify-end">
-                                        <Link href={`/explore?tab=users${search !== undefined ? `&search=${search}` : ''}`} className="flex items-center text-sm text-muted-foreground font-medium">
-                                             More
-                                        </Link>
-                                   </CardFooter>
-                                   </Card>
-                              )
-                         }
                          {tab === 'posts' && posts.length === 0 && search && (
                               <div className="flex flex-col items-center justify-center w-full">
                                    <EmptyPlaceholder>
---
+++ components/explore/tab-content.tsx
@@ -0,0 +1,129 @@
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
+import Link from "next/link"
+import { Icons } from "../icon"
+import UserHoverCard from "../user-hover-card"
+import { Hash } from "lucide-react"
+import { cn } from "@/lib/utils"
+
+export default function ExploreComponent({users, posts, tags, session, search, tab, activeTab, searchParams, className}: {
+     users: any, posts: any, tags: any, session: any, search: any, tab: any, activeTab: any, searchParams: any, className?: string
+}) {
+     return (
+          <div className={cn("w-full mb-10 space-y-4", className)}>
+               {posts.length > 0 && (
+               <Card>
+                    <CardHeader className="">
+                         <CardTitle className="feed__content_featured_card_title text-base">Posts</CardTitle>
+                    </CardHeader>
+                    <CardContent className="">
+                         <ol className="flex flex-col items-start justify-between space-y-4">
+                              {posts.map(
+                                   (item: any, index: number) => (
+                                        <li key={item.id} className="text-sm space-y-2.5">
+
+                                             <Link href={`/${item.author.username}`} className="text-xs flex items-center mb-2 font-medium">
+                                                  <Avatar className="h-5 w-5 mr-1 md:mr-1.5 ">
+                                                       <AvatarImage src={item.author?.image} alt={item.author?.username} />
+                                                       <AvatarFallback>{item.author?.name?.charAt(0) || item.author?.username?.charAt(0)}</AvatarFallback>
+                                                  </Avatar>
+                                                  {item.author.name || item.author.username} {item.author?.verified && (
+                                                       <Icons.verified className="h-3 w-3 mx-0.5 inline fill-primary align-middle" />
+                                                  )}
+                                             </Link>
+
+
+                                             <Link href={`/${item.author.username}/${item.url}`} className="text-base font-bold line-clamp-2 overflow-hidden leading-tight">
+                                                  {item.title}
+                                             </Link>
+                                        </li>
+                                   ))}
+                         </ol>
+                    </CardContent>
+                    <CardFooter className="flex justify-end">
+                         <Link href={`/explore?tab=posts${search !== undefined ? `&search=${search}` : ''}`} className="flex items-center text-sm text-muted-foreground font-medium">
+                              More
+                         </Link>
+                    </CardFooter>
+               </Card>
+
+          )}
+          {users.length > 0 && (
+               <Card>
+                    <CardHeader>
+                         <CardTitle className="feed__content_featured_card_title text-base">Users</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                         <div className="feed__content_featured_card_content flex flex-col items-start justify-between space-y-4">
+                              {users?.map(
+                                   (item: any, index: number) => (
+                                        <div className="flex gap-4 w-full items-center justify-between" key={item.id}>
+                                             <div className="space-y-3">
+                                                  <UserHoverCard user={item} >
+                                                       <Link href={`/${item.username}`} className="flex items-center">
+                                                            <Avatar className="mr-1.5 md:mr-2 flex items-center justify-center bg-muted h-8 w-8">
+                                                                 <AvatarImage src={item.image} alt={item.username} />
+                                                                 <AvatarFallback>{item.name?.charAt(0) || item.username?.charAt(0)}</AvatarFallback>
+                                                            </Avatar>
+                                                            {
+                                                                 !item.name ? (
+                                                                      <div>
+                                                                           <p className="text-sm font-medium leading-none">{item.username} {item?.verified && (
+                                                                                <Icons.verified className="h-3 w-3 mx-1 inline fill-primary align-middle" />
+                                                                           )}</p>
+                                                                      </div>
+                                                                 ) : (
+                                                                      <div>
+                                                                           <p className="text-sm font-medium leading-none">{item.name} {item?.verified && (
+                                                                                <Icons.verified className="h-3 w-3 inline fill-primary align-middle" />
+                                                                           )}</p>
+                                                                           <p className="text-sm text-muted-foreground">{item.username}</p>
+                                                                      </div>
+                                                                 )
+                                                            }
+                                                       </Link>
+                                                  </UserHoverCard>
+                                             </div>
+                                        </div>
+                                   ))}
+                         </div>
+                    </CardContent>
+                    <CardFooter className="flex justify-end">
+                         <Link href={`/explore?tab=users${search !== undefined ? `&search=${search}` : ''}`} className="flex items-center text-sm text-muted-foreground font-medium">
+                              More
+                         </Link>
+                    </CardFooter>
+               </Card>
+          )}
+
+          {tags.length > 0 && (
+               <Card>
+                    <CardHeader>
+                         <CardTitle className="text-base">Tags</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                         <div className="flex flex-col divide-y">
+                              {tags?.map((tag: any, index: number) => (
+                                   <div className="flex items-center justify-between py-2.5" key={tag.id}>
+                                        <Link href={`/tags/${tag.name}`} className="w-full">
+                                             <div className="flex items-center">
+                                                  <div className="space-y-1">
+                                                       <p className="text-base capitalize"><Hash className="h-4 w-4 mr-1.5 inline" />{tag.name.replace(/-/g, " ")}</p>
+                                                       <p className="text-sm text-muted-foreground">{tag._count.posts} posts · {tag._count.followingtag} followers</p>
+                                                  </div>
+                                             </div>
+                                        </Link>
+                                   </div>
+                              ))}
+                         </div>
+                    </CardContent>
+                    <CardFooter className="flex justify-end">
+                         <Link href={`/explore?tab=tags${search !== undefined ? `&search=${search}` : ''}`} className="flex items-center text-sm text-muted-foreground font-medium">
+                              More
+                         </Link>
+                    </CardFooter>
+               </Card>
+          )}
+          </div>
+     )
+}
