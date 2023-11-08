aa1878910f6a5e786eb9831ede17361f52e2975e
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-08 13:45:04 +0900

    Remove getForYou function from fetchForYou

--- components/feed/get-foryou.ts
+++
@@ -1,8 +0,0 @@
-import { getForYou } from "@/lib/prisma/feed";
-
-export const fetchForYou = async ({ page = 0 }: { page?: number }) => {
-      const result = await getForYou({ page });
-      
-      return result?.feed;
-}
-
