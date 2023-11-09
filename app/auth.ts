e86d2ad92a5ce21fde1041bf7cb1f52fe1800650
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-09 18:28:30 +0900

    Update NextAuth configuration with new session and signIn pages, and fix import of PrismaAdapter

--- app/auth.ts
+++ app/auth.ts
@@ -1,11 +1,17 @@
 import postgres from "@/lib/postgres"
 import type { NextAuthOptions as NextAuthConfig } from "next-auth"
 import GitHub from "next-auth/providers/github"
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
 export const config = {
   // https://next-auth.js.org/configuration/providers/oauth
-  //adapter: PrismaAdapter(postgres),
+  //adapter: PrismaAdapter(postgres as any),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/signin",
+  },
   providers: [
     GitHub({ 
       clientId: process.env.GITHUB_CLIENT_ID, 
@@ -17,26 +23,26 @@ export const config = {
       
       if (account?.provider === 'github' && user) {
         const { login: username, name, email, bio, html_url: githubProfileURL, avatar_url, location, id: githubId } = profile as any;
-  
+        if (!username) {
+            
+          return false; // Do not continue with the sign-in process
+        }
         console.log("GitHub Profile:", profile);
   
         // Check if the user exists in your database based on their email
         // const userExists = await sql('SELECT * FROM Users WHERE Username = $1;', [username]);
-        const userExists = await postgres.user.findMany({
+        const userExists = await postgres.user.findFirst({
           where: {
             username: username
           }
         })
   
-        if (!userExists[0]) {
-          if (!username) {
-            
-            return false; // Do not continue with the sign-in process
-          }
+        if (!userExists) {
+          
           // User doesn't exist, add them to the Users table
           try {
             // await sql('INSERT INTO Users (Username, Name, Email, Bio, GithubProfileURL, AvatarURL, Location) VALUES ($1, $2, $3, $4, $5, $6, $7);', [username, name, email, bio, githubProfileURL, avatar_url, location]);
-            await postgres.user.create({
+            const sessionUser = await postgres.user.create({
               data: {
                 username: username,
                 name: name,
@@ -46,27 +52,23 @@ export const config = {
                 location: location,
                 image: avatar_url,
                 password: githubId.toString()
-              }
-            })
-
-            // const sessionUser = await sql('SELECT * FROM Users WHERE Username = $1;', [username]);
-            const sessionUser = await postgres.user.findMany({
-              where: {
-                username: username
+              },
+              select: {
+                id: true
               }
             })
 
             //const userSettingsExists = await sql('SELECT * FROM UserSettings WHERE UserId = $1;', [sessionUser[0].userid]);
-            const userSettingsExists = await postgres.userSettings.findMany({
+            const userSettingsExists = await postgres.userSettings.findFirst({
               where: {
-                userId: sessionUser[0].id
+                userId: sessionUser.id
               }
             })
-            if (!userSettingsExists[0]) {
+            if (!userSettingsExists) {
               //await sql('INSERT INTO UserSettings (UserId) VALUES ($1);', [sessionUser[0].userid])
               await postgres.userSettings.create({
                 data: {
-                  userId: sessionUser[0].id
+                  userId: sessionUser.id
                 }
               })
             }
@@ -78,6 +80,43 @@ export const config = {
       }
       return true; // Continue sign-in process
     },
+    async jwt({ token, user }) {
+    const dbUser = await postgres.user.findFirst({
+      where: {
+        OR: [
+          {
+            email: user?.email,
+          },
+          {
+            image: user?.image,
+          },
+        ],
+      },
+    });
+
+    if (!dbUser) {
+      if (user) {
+        token.id = user?.id
+      }
+      return token
+    }
+
+    return {
+      id: dbUser.id,
+      name: dbUser.name,
+      email: dbUser.email,
+      picture: dbUser.image,
+    }
+  },
+  async session({ token, session  }) {
+    if (token && session.user) {
+      session.user.name = token.name
+      session.user.email = token.email
+      session.user.image = token.picture
+    }
+
+    return session;
+  },
   },
   
 } satisfies NextAuthConfig
