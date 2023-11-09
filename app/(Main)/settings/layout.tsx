cb489fbf5b910eda541b0772675a5cfb019cbdee
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-09 13:34:18 +0900

    Add padding to settings layout

--- app/(Main)/settings/layout.tsx
+++ app/(Main)/settings/layout.tsx
@@ -39,7 +39,7 @@ interface SettingsLayoutProps {
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
     <>
-      <div className="space-y-6">
+      <div className="space-y-6 p-10 pb-16">
         <div className="space-y-0.5">
           <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
           <p className="text-muted-foreground">
