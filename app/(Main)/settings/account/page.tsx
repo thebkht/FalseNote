dc595c9a47c8b573f2b2fdeaa30aa551b9bedb4b
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-10 15:25:02 +0900

    update

--- app/(Main)/settings/account/page.tsx
+++ app/(Main)/settings/account/page.tsx
@@ -18,7 +18,7 @@ export default async function SettingsAccountPage() {
         </p>
       </div>
       <Separator />
-      <AccountForm data={userData} />
+      <AccountForm />
     </div>
   )
 }
