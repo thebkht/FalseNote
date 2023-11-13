a54dc45ea7b302dfef6e7c035f281cfbcd55514f
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-13 22:44:50 +0900

    Update max-width for loading component

--- app/(Main)/[username]/[url]/loading.tsx
+++ app/(Main)/[username]/[url]/loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 export default function Loading() {
   return (
     <div className="grid w-full gap-10">
-      <div className="mx-auto lg:w-[800px] w-full space-y-6">
+      <div className="mx-auto max-w-[650px] lg:max-w-[680px] w-full space-y-6">
         <Skeleton className="h-[50px] w-full" />
         <Skeleton className="h-[20px] w-2/3" />
         <Skeleton className="h-[20px] w-full" />
