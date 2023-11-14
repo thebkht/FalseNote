32d9b76ce50876e58319d0b0a7a21f7efc46ad8b
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-14 21:47:20 +0900

    Add top margin to loading component

--- app/editor/loading.tsx
+++ app/editor/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export default function Loading() {
   return (
-    <div className="grid w-full gap-10">
+    <div className="grid w-full gap-10 mt-16">
       <div className="mx-auto lg:w-[800px] w-full space-y-6">
         <Skeleton className="h-[50px] w-full" />
         <Skeleton className="h-[20px] w-2/3" />
