6999dbc298a7141593c136796d7a10a50867482e
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-14 22:32:23 +0900

    Refactor date formatting to use shared function

--- components/landing/landing.tsx
+++ components/landing/landing.tsx
@@ -13,41 +13,10 @@ import { Icons } from "../icon";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import useWindowDimensions from "../window-dimensions";
 import { SiteFooter } from "../footer";
-
-export function dateFormat(dateString: string | number | Date) {
-  const date = new Date(dateString);
-  const currentDate = new Date();
-
-  const differenceInTime = currentDate.getTime() - date.getTime();
-  const differenceInDays = differenceInTime / (1000 * 3600 * 24);
-
-  if (differenceInDays < 1) {
-    const differenceInHours = differenceInTime / (1000 * 3600);
-    if (differenceInHours < 1) {
-      const differenceInMinutes = differenceInTime / (1000 * 60);
-      if (differenceInMinutes < 1) {
-        const differenceInSeconds = differenceInTime / 1000;
-        return differenceInSeconds < 1 ? 'Just now': `${Math.floor(differenceInSeconds)}s`;
-      }
-      return `${Math.floor(differenceInMinutes)}m`;
-    }
-    return `${Math.floor(differenceInHours)}h`;
-  }
-
-  if (differenceInDays > 30) {
-    return `${date.toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: 'short',
-      day: 'numeric',
-    })}`;
-  } else {
-    return `${Math.floor(differenceInDays)}d`;
-  }
-}
+import { dateFormat } from "@/lib/format-date";
 
 export default function Landing({ latest, tags, popular }: { latest: any, tags: any, popular: any }) {
   const { width, height } = useWindowDimensions();
-  console.log(width)
   return (
     <>
       <main className="landing mx-auto w-full overflow-hidden">
