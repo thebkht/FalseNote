6420553b3e790ecefc72a350a118af29d88a7a1a
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-17 06:25:04 +0900

    Update next dependency version to 14.0.3

--- app/get-started/page.tsx
+++ app/get-started/page.tsx
@@ -1,10 +1,12 @@
 import { getSessionUser } from "@/components/get-session-user";
 import TagsDialog from "@/components/get-started/tags";
 import { getTags } from "@/lib/prisma/tags";
+import { redirect } from "next/navigation";
 
 export default async function GetStartedPage() {
      const session = await getSessionUser();
      const { tags } = await getTags({id: session?.id});
+     !session && redirect('/signin');
      return (
           <>
                <TagsDialog tags={tags} session={session} />
