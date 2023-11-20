16bc4494fee4aded7fa3d297fe53e1aa2c1702aa
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-21 01:14:52 +0900

    Fix image upload and tag addition in PostEditorForm

--- components/editor/post-editor-form.tsx
+++ components/editor/post-editor-form.tsx
@@ -460,7 +460,7 @@ export function PostEditorForm(props: { post: any, user: any }) {
                           <>
                             
                             <div className="flex items-center justify-center w-full">
-                              <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-dashed rounded-md cursor-pointer bg-popover/50">
+                              <label className="flex flex-col items-center justify-center w-full h-64 border border-dashed rounded-md cursor-pointer bg-popover/50">
                               {
                               cover ? (
                                 <AspectRatio ratio={16 / 9} className="bg-muted rounded-md">
@@ -474,10 +474,10 @@ export function PostEditorForm(props: { post: any, user: any }) {
                               ) : (
                             
                                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                  <svg className="w-8 h-8 mb-4 text-secondary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                  <svg className="w-9 h-9 mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
                                   </svg>
-                                  <p className="mb-2 text-sm text-secondary-foreground"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                                  <p className="mb-2 text-sm text-secondary-foreground font-medium">Click to upload</p>
                                   <p className="text-xs text-secondary-foreground">PNG, JPG (MAX. 2MB)</p>
                                 </div> )
                                 }
@@ -548,7 +548,7 @@ export function PostEditorForm(props: { post: any, user: any }) {
                             </TagBadge>
                           ))}
                         </div>
-                        {fields.length !== 5 && (
+                        {fields.length < 5 && (
                           <FormControl>
                             <>
 
@@ -592,7 +592,7 @@ export function PostEditorForm(props: { post: any, user: any }) {
                       className="mt-2"
                       onClick={() => {
                         if (newTag?.trim() !== '' && newTag !== undefined) {
-                          append({ value: newTag });
+                          append({ value: newTag.toLowerCase() });
                           setNewTag('');
                         }
                       }}
