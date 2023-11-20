efff959c53e439a936b7e9e36e723f117eaed250
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-20 19:21:09 +0900

    Fix type mismatch in fetchUsers and fetchFollowingTags functions

--- components/feed/fetch-user.ts
+++ components/feed/fetch-user.ts
@@ -2,7 +2,7 @@ import postgres from "@/lib/postgres";
 import { getSessionUser } from "../get-session-user";
 import { getFollowings } from "@/lib/prisma/session";
 
-export const fetchUsers = async ({id, limit = 3} : {id?: number | undefined, limit?: number}) => {
+export const fetchUsers = async ({id, limit = 3} : {id?: string | undefined, limit?: number}) => {
 
   const topUsers = await postgres.user.findMany({
     include: {
