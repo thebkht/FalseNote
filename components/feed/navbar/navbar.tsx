430a0d6f93034e11c8eabc96402f23a966a26d07
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-21 07:36:14 +0900

    Update feed navbar styling

--- components/feed/navbar/navbar.tsx
+++ components/feed/navbar/navbar.tsx
@@ -31,12 +31,12 @@ export default function FeedTabs({ tabs, activeTab = 'foryou', children }: { tab
                                    <div ref={firstTab} className="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm bg-transparent">
                                         <Link href={`/explore`}><Plus className="h-5 w-5" /></Link>
                                    </div>
-                                   <div className={`inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ${activeTab == 'foryou' ? 'bg-background shadow-sm text-foreground border-foreground border' : 'bg-muted'}`} onClick={() => {
+                                   <div className={`inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ${activeTab == 'foryou' ? 'bg-secondary-foreground shadow-sm text-foreground' : 'bg-muted'}`} onClick={() => {
                                         router.replace('/feed')
                                    }}>
                                         For You
                                    </div>
-                                   <div className={`inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ${activeTab == 'following' ? 'bg-background shadow-sm text-foreground border-foreground border' : 'bg-muted'}`} onClick={() => {
+                                   <div className={`inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ${activeTab == 'following' ? 'bg-secondary-foreground shadow-sm text-foreground' : 'bg-muted'}`} onClick={() => {
                                         router.replace('/feed?tab=following')
                                    }}>
                                         Following
