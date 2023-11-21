9673e47dc8708e792237f9884a85f78d8293a53e
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-21 13:42:23 +0900

    Fix aspect ratio and border radius in PostCardSkeleton

--- components/skeletons/feed-post-card.tsx
+++ components/skeletons/feed-post-card.tsx
@@ -33,7 +33,7 @@ export default function PostCardSkeleton(
                         </div>
                       </div>
                     </div>
-                    <Skeleton className="h-14 md:h-28 aspect-[4/3] md:aspect-square ml-6 md:ml-8 rounded-none" />
+                    <Skeleton className="h-14 md:h-28 aspect-[4/3] rounded-md md:aspect-square ml-6 md:ml-8" />
                   </div>
                 </CardContent>
           </Card>
