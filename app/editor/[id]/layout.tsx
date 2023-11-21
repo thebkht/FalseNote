d60808adf40243ac57cffc5800ad967c1685e56e
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-22 08:52:59 +0900

    Update editor metadata

---
+++ app/editor/[id]/layout.tsx
@@ -0,0 +1,57 @@
+import { ScrollArea } from '@/components/ui/scroll-area'
+import EditorNavbar from '@/components/navbar/editor-nav'
+import { Toaster } from '@/components/ui/toaster'
+import type { Metadata } from 'next'
+import postgres from '@/lib/postgres'
+import { notFound } from 'next/navigation'
+
+type Props = {
+  params: { id: string }
+  children: React.ReactNode
+}
+
+async function getPostData(id: string) {
+  const post = await postgres.post.findFirst({
+    where: { id },
+    include: {
+      tags: {
+        include: {
+          tag: true
+        }
+      },
+    }
+  });
+  return post;
+}
+
+export async function generateMetadata(
+  { params }: Props
+): Promise<Metadata> {
+  const post = await getPostData(params.id);
+    if (!post) {
+      return notFound();
+    }
+    return {
+      metadataBase: new URL(`${process.env.DOMAIN}/editor/${post.id}`),
+      title: `Editing ${post.title} - FalseNotes`,
+      description: `You are currently editing ${post.title}`,
+      openGraph: {
+        title: `Editing ${post.title} - FalseNotes`,
+        description: `You are currently editing ${post.title}`,
+        url: new URL(`${process.env.DOMAIN}/editor/${post.id}`),
+      },
+      twitter: {
+        title: `Editing ${post.title} - FalseNotes`,
+        description: `You are currently editing ${post.title}`,
+            
+      },
+    }
+}
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
