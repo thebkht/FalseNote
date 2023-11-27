8b25b3a52909c38c0fbb71314da549f69864aeef
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-28 03:07:15 +0900

    Update tag filtering in feed.ts

--- lib/prisma/feed.ts
+++ lib/prisma/feed.ts
@@ -161,7 +161,7 @@ const sortedTagIds = Object.entries(tagCounts)
   .map(([tagId]) => tagId)
 
   const posts = await postgres.post.findMany({
-    where: { tags: { some: { tagId: { in: sortedTagIds } } } },
+    where: { tags: { some: { tagId: { in: sortedTagIds.slice(0, 5) } } } },
     select: { id: true },
   });
 
