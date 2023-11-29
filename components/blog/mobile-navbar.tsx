f69b661f7386500d3da9e1f366d2abdfd6584f2c
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-29 15:11:49 +0900

    Add PostAnalyticsDialog component

--- components/blog/mobile-navbar.tsx
+++ components/blog/mobile-navbar.tsx
@@ -14,6 +14,7 @@ import { Separator } from "../ui/separator";
 import { validate } from "@/lib/revalidate";
 import { Post } from "@prisma/client";
 import { Icons } from "../icon";
+import PostAnalyticsDialog from "./post-analytics-dialog";
 
 export default function MobilePostTabs({ post: initialPost, className, session, author, onClicked }: { post: any, className?: string, session: any, author: any, onClicked: () => void }) {
      const [post, setPost] = useState<any>(initialPost);
@@ -25,21 +26,21 @@ export default function MobilePostTabs({ post: initialPost, className, session, 
           await fetch(`/api/post/${postId}/like`, {
                method: "POST",
                headers: {
-                 "Content-Type": "application/json",
+                    "Content-Type": "application/json",
                },
                body: JSON.stringify({ postId }),
-             });
-             await validate(pathname)
+          });
+          await validate(pathname)
      }
      const save = async (postId: Post['id']) => {
           await fetch(`/api/post/${postId}/save`, {
                method: "POST",
                headers: {
-                 "Content-Type": "application/json",
+                    "Content-Type": "application/json",
                },
                body: JSON.stringify({ postId }),
-             });
-             await validate(pathname)
+          });
+          await validate(pathname)
      }
      const isLiked = post?.likes?.some((like: any) => like.authorId === session?.id);
      const isSaved = post?.savedUsers?.some((savedUser: any) => savedUser.userId === session?.id);
@@ -49,7 +50,7 @@ export default function MobilePostTabs({ post: initialPost, className, session, 
                <div className={cn("p-2 border rounded-full shadow-xl flex md:hidden mx-auto w-max sticky bottom-5 bg-background/60 backdrop-blur-md", className)}>
                     <div className="flex items-center justify-between w-full gap-6 px-6">
                          <div className="flex items-center justify-center flex-1">
-                         {
+                              {
                                    session ? (
                                         <Button className="h-10 w-10 mr-0.5 rounded-full hover:bg-primary hover:text-primary-foreground" size={"icon"} variant={"ghost"} onClick={() => like(post.id)} disabled={session.id == post.authorId} >
                                              <Icons.like className={`w-6 h-6 ${isLiked && 'fill-current'}`} />
@@ -68,7 +69,7 @@ export default function MobilePostTabs({ post: initialPost, className, session, 
                          </div>
                          <Separator orientation="vertical" />
                          <div className="flex items-center justify-center flex-1">
-                         <Button className="h-10 w-10 mr-0.5 rounded-full hover:bg-primary hover:text-primary-foreground" size={"icon"} variant={"ghost"} onClick={onClicked}>
+                              <Button className="h-10 w-10 mr-0.5 rounded-full hover:bg-primary hover:text-primary-foreground" size={"icon"} variant={"ghost"} onClick={onClicked}>
                                    <Icons.commentBubble className="w-6 h-6" />
                                    <span className="sr-only">Comment</span>
                               </Button>
@@ -76,27 +77,30 @@ export default function MobilePostTabs({ post: initialPost, className, session, 
                          </div>
                          <Separator orientation="vertical" />
                          <div className="flex items-center justify-center flex-1">
-                         {
-                              session ? (
-                                   <Button className="h-10 w-10 mr-0.5 rounded-full hover:bg-primary hover:text-primary-foreground" size={"icon"} variant={"ghost"} onClick={() => save(post.id)} >
-                                        <Icons.bookmark className={`w-6 h-6 ${isSaved && 'fill-current'}`} />
-                                        <span className="sr-only">Save</span>
-                                   </Button>
-                              ) : (
-                                   <LoginDialog>
-                                        <Button className="h-10 w-10 mr-0.5 rounded-full hover:bg-primary hover:text-primary-foreground" size={"icon"} variant={"ghost"} >
-                                             <Icons.bookmark className="w-6 h-6" />
+                              {
+                                   session ? (
+                                        <Button className="h-10 w-10 mr-0.5 rounded-full hover:bg-primary hover:text-primary-foreground" size={"icon"} variant={"ghost"} onClick={() => save(post.id)} >
+                                             <Icons.bookmark className={`w-6 h-6 ${isSaved && 'fill-current'}`} />
                                              <span className="sr-only">Save</span>
                                         </Button>
-                                   </LoginDialog>
+                                   ) : (
+                                        <LoginDialog>
+                                             <Button className="h-10 w-10 mr-0.5 rounded-full hover:bg-primary hover:text-primary-foreground" size={"icon"} variant={"ghost"} >
+                                                  <Icons.bookmark className="w-6 h-6" />
+                                                  <span className="sr-only">Save</span>
+                                             </Button>
+                                        </LoginDialog>
+                                   )
+                              }
+                         </div>
+                         {
+                              session?.id === post.authorId && (
+                                   <>
+                                        <Separator orientation="vertical" />
+                                        <PostAnalyticsDialog post={post} className='rounded-full hover:bg-primary hover:text-primary-foreground text-background-foreground' />
+                                   </>
                               )
                          }
-                         </div>
-                         {/* <PostMoreActions post={post} session={session} >
-                              <Button className="h-10 w-10 mr-0.5" size={"icon"} variant={"ghost"} >
-                                   <MoreHorizontal className="w-5 h-5" strokeWidth={1.75} />
-                              </Button>
-                         </PostMoreActions> */}
                     </div>
                </div>
                <PostDeleteDialog post={post} user={session} open={showDeleteAlert} onOpenChange={setShowDeleteAlert} />
