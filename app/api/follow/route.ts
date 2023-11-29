107af977437ad2f721c2e24b57027de7fdd442a4
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-11-30 01:37:47 +0900

    Fix notification deletion logic

--- app/api/follow/route.ts
+++ app/api/follow/route.ts
@@ -29,6 +29,33 @@ export async function GET(request: NextRequest) {
         },
       });
 
+      // if followed during 1 week, delete notification
+      const oneWeekAgo = new Date();
+      oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+      if (isFollowed.createdAt > oneWeekAgo) {
+        const notification = await postgres.notification.findFirst({
+          where: {
+            senderId: followerId,
+            receiverId: followeeId,
+            type: "follow",
+          },
+          orderBy: {
+            createdAt: "desc",
+          },
+          select: {
+            id: true,
+          },
+        });
+
+        if (notification) {
+          await postgres.notification.delete({
+            where: {
+              id: notification.id,
+            },
+          });
+        }
+      }
+
       return NextResponse.json({ message: "unfollowed" }, { status: 200 });
     } else {
       await postgres.follow.create({
@@ -40,33 +67,33 @@ export async function GET(request: NextRequest) {
 
       // Check if followerId and followeeId are not null
       if (followerId && followeeId) {
-          //Create notification
-          const sender = await postgres.user.findUnique({
-            where: {
-              id: followerId,
-            },
+        //Create notification
+        const sender = await postgres.user.findUnique({
+          where: {
+            id: followerId,
+          },
+        });
+
+        const receiver = await postgres.user.findUnique({
+          where: {
+            id: followeeId,
+          },
+        });
+
+        // Check if sender and receiver are not null
+        if (sender && receiver) {
+          const message = `${sender?.name || sender?.username} followed you`;
+          const type = "follow";
+          const url = `/@${sender?.username}`;
+          await create({
+            content: message,
+            type,
+            url,
+            receiverId: receiver?.id || "",
+            senderId: sender?.id || "",
           });
-  
-          const receiver = await postgres.user.findUnique({
-            where: {
-              id: followeeId,
-            },
-          });
-          
-          // Check if sender and receiver are not null
-          if (sender && receiver) {
-            const message = `${sender?.name || sender?.username} followed you`;
-            const type = "follow";
-            const url = `/@${sender?.username}`
-            await create({
-              content: message,
-              type,
-              url,
-              receiverId: receiver?.id || "",
-              senderId: sender?.id || "",
-            });
-          }
         }
+      }
 
       return NextResponse.json({ message: "followed" }, { status: 200 });
     }
